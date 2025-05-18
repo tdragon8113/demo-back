b9dd08be662e6d6b537b58519630959697d0a1e4
tangdinglong <gary@cashcepat.id>
2025-05-18 20:16:54 +0800

    接入阿里的微服务配置中心和注册与发现nacos

--- user-service/src/main/java/demo/loong/user/UserApplication.java
+++ user-service/src/main/java/demo/loong/user/UserApplication.java
@@ -1,16 +1,17 @@
 package demo.loong.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@ServletComponentScan
+@EnableDiscoveryClient
+@MapperScan(basePackages = "demo.loong.user.repository")
 public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
-        System.out.println("Hello World!");
     }
 
 }

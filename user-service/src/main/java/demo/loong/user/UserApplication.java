0ba28ec81d5ce84382421a14efc9ccb96d887d13
tangdinglong <tdragon8113@qq.com>
2025-06-09 15:02:27 +0800

    优化配置

--- user-service/src/main/java/demo/loong/user/UserApplication.java
+++ user-service/src/main/java/demo/loong/user/UserApplication.java
@@ -1,5 +1,6 @@
 package demo.loong.user;
 
+import cn.hutool.core.date.StopWatch;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +12,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class UserApplication {
 
     public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         SpringApplication.run(UserApplication.class, args);
-
+        stopWatch.stop();
+        System.out.println("花费：" + stopWatch.getTotalTimeMillis());
     }
 
 }
---
+++ user-service/src/test/java/demo/loong/user/test/ProxyTest.java
@@ -0,0 +1,14 @@
+package demo.loong.user.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProxyTest {
+
+    @Test
+    public void proxy() {
+
+
+    }
+}
